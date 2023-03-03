e161e3e44d9ac092d357ab2bb083542078635fa3
Janak Mallik <janakmallik20@gmail.com>
2023-03-04 05:00:17 +0600

    new up

---
+++ blogs/function2.js
@@ -0,0 +1,93 @@
+
+var button = document.getElementById("night_mode_btn");
+
+function toggleMode() {
+    var body = document.getElementsByTagName("body")[0];
+    var button = document.getElementsByTagName("button")[0];
+
+    body.classList.toggle("night-mode");
+    button.classList.toggle("night-mode");
+
+    if (button.innerHTML === "Night Mode") {
+        button.innerHTML = "Day Mode";
+    } else {
+        button.innerHTML = "Night Mode";
+    }
+}
+
+function toggleNavbar() {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('show');
+  }
+  
+
+  function goBack() {
+    window.history.back();
+  }
+
+
+
+//   night and day mode
+var text = "Dark Mode";
+
+function toggleMode() {
+    var body = document.getElementsByTagName("body")[0];
+    var button = document.getElementsByTagName("button")[0];
+
+    
+
+    body.classList.toggle("night-mode");
+    button.classList.toggle("night-mode");
+
+    //text = button.innerHTML;
+
+    if (text === "Dark Mode") {
+        text = "Light Mode";
+    button.innerHTML = "Light Mode";
+
+    } else {
+    button.innerHTML = "Dark Mode";
+    text = "Dark Mode";
+    }
+};
+
+
+// back button
+
+function goBack() {
+    window.history.back();
+  };
+
+
+  //date and time
+  function updateClock() {
+  var now = new Date();
+  var time = document.getElementById("time");
+  var date = document.getElementById("date");
+  
+  // Format the time as HH:MM:SS AM/PM
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  seconds = seconds < 10 ? '0'+seconds : seconds;
+  var timeString = hours + ':' + minutes + ' ' + ampm;
+//   var timeString = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  time.innerHTML = timeString;
+  
+  // Format the date as Weekday, Month Day Year
+  var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var weekdayString = weekdays[now.getDay()];
+  var monthString = months[now.getMonth()];
+  var dateString = weekdayString + ", " + now.getDate() + " "+ monthString + " " + " " + now.getFullYear();
+  date.innerHTML = dateString;
+}
+
+// Call updateClock every second to update the time and date
+setInterval(updateClock, 1000);
+
+  
